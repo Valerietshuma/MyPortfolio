c1d621f39f2deb9633698f9f4c6d338ea2741e32
valerie Tshuma <hlulanivalerietshuma@gmail.com>
2024-10-28 11:49:58 +0200

    Made chnages on the navigation and added a hamburger with a backround since it was plain, i also fixed it to be media friendly and not overlap when on media mode

--- PortF/css/nav.js
+++ PortF/css/nav.js
@@ -1,7 +1,29 @@
+
+
+function toggleMenu() {
+    const navMenu = document.getElementById('mainListDiv');
+    navMenu.classList.toggle('show_list'); // Toggle the class for showing/hiding the menu
+}
+
+// Optionally, if using jQuery:
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
+});
+let lastScrollTop = 0; // Keep track of the last scroll position
+const nav = document.querySelector('.nav'); // Get the navigation element
 
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        nav.classList.add('hidden'); // Hide nav
+    } else {
+        // Scrolling up
+        nav.classList.remove('hidden'); // Show nav
+    }
+    lastScrollTop = scrollTop; // Update the last scroll position
 });
